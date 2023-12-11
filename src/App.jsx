b2d53f2a0811fbb22ddc1f66767501685ae307a7
Ben Aderaw <asyncba@gmail.com>
2023-12-11 17:00:06 -0500

    created the main pages > added routes to the different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PagenotFound";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
