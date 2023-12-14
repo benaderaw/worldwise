71fe1f568f4e8ac790d929c4b2be920672905460
Ben Aderaw <asyncba@gmail.com>
2023-12-14 15:21:26 -0500

    created nested routes for cities and countries tab

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Cities tracked</p>} />
+          <Route path="cities" element={<p>Cities tracked</p>} />
+          <Route path="countries" element={<p>Countries Tracked</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
